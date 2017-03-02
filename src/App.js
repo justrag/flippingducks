4f99d74b0234710bbe1b5152bf355697b7d57702
Just RAG <justrag@gmail.com>
2017-03-02 19:47:03 +0100

    prettier reformat

--- src/App.js
+++ src/App.js
@@ -1,47 +1,58 @@
-import React, { Component } from 'react';
-import classNames from 'classnames';
-import FlipMove from 'react-flip-move';
-import './App.css';
+import React, { Component } from "react";
+import classNames from "classnames";
+import FlipMove from "react-flip-move";
+import "./App.css";
 
 const dieRoll = () => 1 + Math.floor(Math.random() * 6);
 
 class App extends Component {
-constructor(props) {
-super(props);
-this.state= {1: 6, 2: 6, 3: 6, 4: 6, 5: 6, 6:6};
-}
+  constructor(props) {
+    super(props);
+    this.state = { 1: 6, 2: 6, 3: 6, 4: 6, 5: 6, 6: 6 };
+  }
+
+  renderDucks = () =>
+    [1, 2, 3, 4, 5, 6].map(d => (
+      <Duck key={d} duckId={d} pond={this.state[d]} pondseq={1} />
+    ));
+
   render() {
     return (
-<div>
-<FlipMove delay={500} duration={5000} easing="ease-out" staggerDurationBy={200}>
-{[1,2,3,4,5,6].map(d => (
-<Duck key={d} id={d} pond={this.state[d]} pondseq={1} />
-))}
-</FlipMove>
+      <div>
+        <FlipMove
+          delay={500}
+          duration={5000}
+          easing="ease-out"
+          staggerDurationBy={200}
+        >
+          {this.renderDucks()}
+        </FlipMove>
+        {[0, 1, 2, 3, 4, 5, 6].map(pond => <Pond key={pond} pondId={pond} />)}
 
-{[0,1,2,3,4,5,6].map(pond => (
-<Pond key={pond} id={pond} />
-))}
-
-<button onClick={() => this.setState({[dieRoll()]: dieRoll()})}>CLICK</button>
-</div>
+        <button onClick={() => this.setState({ [dieRoll()]: dieRoll() })}>
+          SHUFFLE
+        </button>
+      </div>
     );
   }
 }
 
 export default App;
 
-const Pond = ({id}) => (
-<div className={classNames('pond',`p${id}`)}></div>
-);
+const Pond = ({ pondId }) => <div id={`pond${pondId}`} className={classNames("pond", `p${pondId}`)} />;
 
 // Needs to be not-stateless for FlipMove
 class Duck extends Component {
-render() {
-const {id, pond, pondseq} = this.props;
-return (
-<div className="duck" style={{zIndex: id, left: `${2+pond*14}vmin`}}>{id}</div>
-);
+  render() {
+    const { duckId, pond, pondseq } = this.props;
+    return (
+      <div
+        id={`duck${duckId}`}
+        className="duck"
+        style={{ zIndex: duckId, left: `${2 + pond * 14}vmin` }}
+      >
+        <span>{duckId}</span>
+      </div>
+    );
+  }
 }
-};
-
